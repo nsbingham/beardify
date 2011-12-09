9a9c4622aa07094c56cc68828d8f1b2413a05abf
Nathan <nathan@bamads.com>
2011-12-09 12:03:01 -0500

    Separated out FaceAPI from FaceJS

--- js/script.js
+++ js/script.js
@@ -1,42 +1,53 @@
+// New idea is to add santa had and beard to
+// Wrap this into a jQuery plugin
+// Wrap this in a jQuery bookmarklet
+// Swap to place based on center of image
+
 $(function(){
 
 	var $body = $('body'),
 	$beards = {
 		'beard1':{scale:1.46,pctL:0.18,pctT:0.28,src:'img/beard.png'},
 		'beard2':{scale:1.46,pctL:0.14,pctT:0.29,src:'img/mustache.png'}
-	};
+	},
+	beard = $beards.beard1;
 
 	$body.find('img').each(function(index, el){
+
 		var $el = $(this), pos = $el.offset(), w = $el.width(), l = $el.height();
 
+		// Only apply to image that are larger than 40x40
 		if(w > 40 && l > 40) {
-			$el.faceDetection(
-				{complete: function(image, coordinates) {
-					beardMe($el,$beards.beard1,image,coordinates);
-					//showOutline($el, image, coordinates);
-				},
-				error: function() {
-					console.log($el);
-					console.log("Could not process image");
-			}});
+			//beardMeJS($el, beard);
+			beardMeFaceApi($el, beard);
+			//showOutline($el, image, coordinates);
 		}
 
 	});
 
 	// JavaScript face detection
-	function beardMe($el, b, image, coordinates){
-		if(coordinates.length) {
-			coordinates.forEach(function(coord) {
-				var $beard = $('<img src="' + b.src + '" />'),
-					bw = Math.round(b.scale * coord.width),
-					bl = Math.round(coord.positionX - (bw*b.pctL)),
-					bt = Math.round((coord.height * b.pctT) + coord.positionY);
+	function beardMeJS($el,beard){
 
-					$beard
-						.css({top:bt, left:bl, width:bw, position:'absolute'})
-						.appendTo($body);
-			});
-		}
+		$el.faceDetection(
+			{complete: function(image, coordinates) {
+				if(coordinates.length) {
+					coordinates.forEach(function(coord) {
+						var $beard = $('<img src="' + beard.src + '" />'),
+							bw = Math.round(beard.scale * coord.width),
+							bl = Math.round(coord.positionX - (bw*beard.pctL)),
+							bt = Math.round((coord.height * beard.pctT) + coord.positionY);
+
+							$beard
+								.css({top:bt, left:bl, width:bw, position:'absolute'})
+								.appendTo($body);
+					});
+				}
+			},
+			error: function() {
+				console.log($el);
+				console.log("Could not process image");
+		}});
+
 	}
 
 	function showOutline($el, image, coordinates){
@@ -54,22 +65,45 @@ $(function(){
 		});
 	}
 
-	function beardMeFaceApi(){
+	function beardMeFaceApi($el, beard){
 
 		//var apiUrl = "http://api.face.com/faces/detect.json?api_key=aa310f793c57fdaebe185bdcee13857&urls=";
 		var apiUrl = "http://local.beardify.com/example.json";
 
 		$.ajax({
-		  url: apiUrl,
-		  dataType: 'jsonp',
-		  data: data,
-		  success: callback
+			url: apiUrl,
+			dataType: 'jsonp',
+			success: beardMeFaceApiCallback,
+			error: beardMeFaceApiError
 		});
 
 	}
 
+	function beardMeFaceApiError(e){
+		console.log(e);
+	}
+
 	function beardMeFaceApiCallback(data, textStatus, jqXHR){
+		log(data,textStatus, jqXHR);
 		console.log('success');
+		if(data.photos !== null) {
+			data.photos.each();
+		}
+	}
+
+	function beardMeFaceApiPlace(index, photo){
+		var $beard = $('<img src="' + beard.src + '" />'),
+			bw = Math.round(beard.scale * coord.width),
+			bl = Math.round(coord.positionX - (bw*beard.pctL)),
+			bt = Math.round((coord.height * beard.pctT) + coord.positionY);
+
+			$beard
+				.css({top:bt, left:bl, width:bw, position:'absolute'})
+				.appendTo($body);
+	}
+
+	function beardMe(x, y){
+		
 	}
 
 });
