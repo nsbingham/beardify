3ea11f8156b68705c6142dd37a51d10bdcd7c5c7
Nathan <nathan@bamads.com>
2011-12-12 11:00:30 -0500

    Beginning Face API integration.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ $(function(){
 
 	var $body = $('body'),
 	$beards = {
-		'beard1':{scale:1.46,pctL:0.18,pctT:0.28,src:'img/beard.png'},
-		'beard2':{scale:1.46,pctL:0.14,pctT:0.29,src:'img/mustache.png'}
+		'beard1':{scale:1.3,pctX:1.02,pctY:0.23,src:'img/beard.png'},
+		'beard2':{scale:1.3,pctX:1.14,pctY:0.29,src:'img/mustache.png'}
 	},
-	beard = $beards.beard1;
+	beard = $beards.beard2;
 
 	$body.find('img').each(function(index, el){
 
@@ -18,9 +18,8 @@ $(function(){
 
 		// Only apply to image that are larger than 40x40
 		if(w > 40 && l > 40) {
-			//beardMeJS($el, beard);
-			beardMeFaceApi($el, beard);
-			//showOutline($el, image, coordinates);
+			beardMeJS($el, beard);
+			//beardMeFaceApi($el, beard);
 		}
 
 	});
@@ -32,14 +31,9 @@ $(function(){
 			{complete: function(image, coordinates) {
 				if(coordinates.length) {
 					coordinates.forEach(function(coord) {
-						var $beard = $('<img src="' + beard.src + '" />'),
-							bw = Math.round(beard.scale * coord.width),
-							bl = Math.round(coord.positionX - (bw*beard.pctL)),
-							bt = Math.round((coord.height * beard.pctT) + coord.positionY);
-
-							$beard
-								.css({top:bt, left:bl, width:bw, position:'absolute'})
-								.appendTo($body);
+							var x = Math.round((coord.width/2) + coord.positionX),
+								y = Math.round((coord.height/2) + coord.positionY);
+							beardMe(x,y,coord.width,coord.height);
 					});
 				}
 			},
@@ -50,7 +44,7 @@ $(function(){
 
 	}
 
-	function showOutline($el, image, coordinates){
+	function showOutline($el, coordinates){
 		coordinates.forEach(function(coord) {
 			$("<div>", {
 				css: {
@@ -102,8 +96,22 @@ $(function(){
 				.appendTo($body);
 	}
 
-	function beardMe(x, y){
-		
+	function beardMe(x, y, w, h){
+
+		var $beard = $('<img src="' + beard.src + '" />'),
+		beardWidth = Math.round(beard.scale * w),
+		beardHeight = Math.round(beard.scale * h),
+		beardCenterX = beardWidth/2,
+		beardCenterY = beardHeight/2,
+		beardX = Math.round(x - (beardCenterX * 1.02)), // increase goes left
+		beardY = Math.round(y - (beardCenterY * .23)); // increase goes up
+
+		log(beardX, beardY);
+
+		$beard
+			.css({top:beardY, left:beardX, width:beardWidth, position:'absolute', zIndex:9999})
+			.appendTo($body);
+
 	}
 
 });
